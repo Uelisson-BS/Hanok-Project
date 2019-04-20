40789f2ce94f2d64f58c6d8a124f6edb45068c65
Uelisson-BS <35790317+Uelisson-BS@users.noreply.github.com>
2019-04-20 16:41:02 -0300

    Update ahlist.js

--- bet/ahlist.js
+++ bet/ahlist.js
@@ -39,6 +39,72 @@
     `  
   }
   nav.innerHTML = navHTML
+	
+		// 取得資料
+  axios.get(INDEX_URL2)
+    .then((response) => {
+      rawData = response.data.results
+      // 預設 hilight Action
+      nav.firstElementChild
+          .firstElementChild.classList.add('active')
+      
+      const filterAction = filterDataByGenres(1)
+      displayMovies(filterAction)
+    })
+    .catch((err) => console.log(err))
+  
+  function displayMovies(data) {
+    let contentHTML = ``
+    data.forEach( item => {
+      contentHTML += `
+        <div class="col-4 mb-3">
+          <div class="card">
+            <img src="${BASE_URL}${POSTERS}${item.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${item.title}</h5>
+              ${displayGenres(item.genres)}
+            </div>
+          </div>
+        </div>
+      `
+    })
+    displayPanel.innerHTML = contentHTML
+  }
+  
+  function displayGenres(array) {
+    let genresHTML = ``
+    array.forEach(item => {
+      genresHTML += `
+        <span class="badge badge-secondary">${genres[item]}</span>
+      `
+    })
+    return genresHTML
+  }
+  
+  function filterDataByGenres(genresNumber) {
+    const genresId = Number(genresNumber)
+    console.log(genresId)
+    const result = rawData.filter( item => { 
+      // 電影是否包含該類型
+      const isGenres = item.genres.some( item => { return item === genresId} )
+      return isGenres })
+    return result
+  }
+  
+  // hilight 所選的導覽項目
+  nav.addEventListener('click',() => {
+    // 先清除所有 active class
+    const navLinkArray = document.querySelectorAll('.nav-link')
+    navLinkArray.forEach( item => {
+      item.classList.remove('active')
+    })
+    // hilight 選項
+    event.target.classList.add('active')
+    // filter display
+    const genresId = event.target.dataset.id
+    const filterData = filterDataByGenres(genresId)
+    displayMovies(filterData)
+  })
 
   const searchBtn = document.getElementById('submit-search')
   const searchInput = document.getElementById('search')
@@ -106,71 +172,6 @@
     dataPanel.innerHTML = htmlContent
  }    
 	
-	// 取得資料
-  axios.get(INDEX_URL2)
-    .then((response) => {
-      rawData = response.data.results
-      // 預設 hilight Action
-      nav.firstElementChild
-          .firstElementChild.classList.add('active')
-      
-      const filterAction = filterDataByGenres(1)
-      displayMovies(filterAction)
-    })
-    .catch((err) => console.log(err))
-  
-  function displayMovies(data) {
-    let contentHTML = ``
-    data.forEach( item => {
-      contentHTML += `
-        <div class="col-4 mb-3">
-          <div class="card">
-            <img src="${BASE_URL}${POSTERS}${item.image}" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">${item.title}</h5>
-              ${displayGenres(item.genres)}
-            </div>
-          </div>
-        </div>
-      `
-    })
-    displayPanel.innerHTML = contentHTML
-  }
-  
-  function displayGenres(array) {
-    let genresHTML = ``
-    array.forEach(item => {
-      genresHTML += `
-        <span class="badge badge-secondary">${genres[item]}</span>
-      `
-    })
-    return genresHTML
-  }
-  
-  function filterDataByGenres(genresNumber) {
-    const genresId = Number(genresNumber)
-    console.log(genresId)
-    const result = rawData.filter( item => { 
-      // 電影是否包含該類型
-      const isGenres = item.genres.some( item => { return item === genresId} )
-      return isGenres })
-    return result
-  }
-  
-  // hilight 所選的導覽項目
-  nav.addEventListener('click',() => {
-    // 先清除所有 active class
-    const navLinkArray = document.querySelectorAll('.nav-link')
-    navLinkArray.forEach( item => {
-      item.classList.remove('active')
-    })
-    // hilight 選項
-    event.target.classList.add('active')
-    // filter display
-    const genresId = event.target.dataset.id
-    const filterData = filterDataByGenres(genresId)
-    displayMovies(filterData)
-  })
 
   function showMovie (id) {
     // get elements
