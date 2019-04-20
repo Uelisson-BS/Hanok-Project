10ef9b81cf3e972634d9c9631f5dfae547ea04a5
Uelisson-BS <35790317+Uelisson-BS@users.noreply.github.com>
2019-04-20 04:26:06 -0300

    Update ahlist.js

--- bet/ahlist.js
+++ bet/ahlist.js
@@ -1,24 +1,9 @@
 (function () {
   const BASE_URL = 'https://uelisson-bs.github.io'
-  const INDEX_URL = BASE_URL + '/Hanok-Project/assets/AH-List/db-list.json'
+  const INDEX_URL = BASE_URL + '/Hanok-Project/bet/movies.json'
   const INDEX_URL2 = BASE_URL + '/Hanok-Project/assets/AH-List/Post-id/'
   const POSTER_URL = 'https://'
   const data = []
-
-  const searchBtn = document.getElementById('submit-search')
-  const searchInput = document.getElementById('search')
-
-  const pagination = document.getElementById('pagination')
-  const ITEM_PER_PAGE = 8
-  
-  const listModel = document.getElementById("btn-listModel")
-  const cardModel = document.getElementById("btn-cardModel")
-  
-  // 設定一個判斷Model的Boolean
-  let isListModel = false
-  // 將頁數預設在第一頁
-  let page = 1
-  
   const displayPanel = document.querySelector('.display-panel')
   const nav = document.querySelector('.nav')
   const genres = {
@@ -53,6 +38,21 @@
       </li>
     `  
   }
+  nav.innerHTML = navHTML
+
+  const searchBtn = document.getElementById('submit-search')
+  const searchInput = document.getElementById('search')
+
+  const pagination = document.getElementById('pagination')
+  const ITEM_PER_PAGE = 8
+  
+  const listModel = document.getElementById("btn-listModel")
+  const cardModel = document.getElementById("btn-cardModel")
+  
+  // 設定一個判斷Model的Boolean
+  let isListModel = false
+  // 將頁數預設在第一頁
+  let page = 1
 
   const dataPanel = document.getElementById('data-panel')
   
