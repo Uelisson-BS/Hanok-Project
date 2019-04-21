9baebf868c1449f5bc71e3e3d41d796ee7495729
Uelisson-BS <35790317+Uelisson-BS@users.noreply.github.com>
2019-04-20 23:31:02 -0300

    Update ahlist.js

--- bet/ahlist.js
+++ bet/ahlist.js
@@ -4,21 +4,8 @@
   const INDEX_URL2 = BASE_URL + '/Hanok-Project/assets/AH-List/Post-id/'
   const POSTER_URL = 'https://'
   const data = []
-
-  const searchBtn = document.getElementById('submit-search')
-  const searchInput = document.getElementById('search')
-
-  const pagination = document.getElementById('pagination')
-  const ITEM_PER_PAGE = 8
   
-  const listModel = document.getElementById("btn-listModel")
-  const cardModel = document.getElementById("btn-cardModel")
   
-  // 設定一個判斷Model的Boolean
-  let isListModel = false
-  // 將頁數預設在第一頁
-  let page = 1
-
   const dataPanel = document.getElementById('data-panel')
 
   const nav = document.querySelector('.nav')
@@ -43,6 +30,20 @@
                   "18": "War",
                   "19": "Western"
                 }
+
+  const searchBtn = document.getElementById('submit-search')
+  const searchInput = document.getElementById('search')
+
+  const pagination = document.getElementById('pagination')
+  const ITEM_PER_PAGE = 8
+  
+  const listModel = document.getElementById("btn-listModel")
+  const cardModel = document.getElementById("btn-cardModel")
+  
+  // 設定一個判斷Model的Boolean
+  let isListModel = false
+  // 將頁數預設在第一頁
+  let page = 1
 
   axios.get(INDEX_URL).then((response) => {
     data.push(...response.data.results)
