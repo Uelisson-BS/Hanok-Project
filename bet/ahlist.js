c9f53aa4238fdfa5c35d9babf18b3fdce433a180
Uelisson-BS <35790317+Uelisson-BS@users.noreply.github.com>
2019-04-20 23:16:23 -0300

    Update ahlist.js

--- bet/ahlist.js
+++ bet/ahlist.js
@@ -21,30 +21,7 @@
 
   const dataPanel = document.getElementById('data-panel')
   
-  const displayPanel = document.querySelector('.display-panel')
-  const nav = document.querySelector('.nav')
-  const genres = {
-                  "1": "Action",
-                  "2": "Adventure",
-                  "3": "Animation",
-                  "4": "Comedy",
-                  "5": "Crime",
-                  "6": "Documentary",
-                  "7": "Drama",
-                  "8": "Family",
-                  "9": "Fantasy",
-                  "10": "History",
-                  "11": "Horror",
-                  "12": "Music",
-                  "13": "Mystery",
-                  "14": "Romance",
-                  "15": "Science Fiction",
-                  "16": "TV Movie",
-                  "17": "Thriller",
-                  "18": "War",
-                  "19": "Western"
-                }
-  let rawData = []
+
   
   // 顯示導覽列
   let navHTML = ``
