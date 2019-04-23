678ab01d75b0c897735200de167b926fc474659f
Uelisson-BS <welisson55@hotmail.com>
2019-04-23 15:05:03 -0300

    Update toggle-box.js

--- assets/AH-Pag/toggle-box.js
+++ assets/AH-Pag/toggle-box.js
@@ -1,1 +1,1 @@
-jQuery(document).ready(function(){jQuery(".toggle-box").hide();jQuery(".toggle").toggle(function(){jQuery(this).addClass("toggle-active")},function(){});jQuery(".toggle").click(function(){jQuery(this).next(".toggle-box")
+jQuery(document).ready(function(){jQuery(".toggle-box").hide();jQuery(".toggle").toggle(function(){jQuery(this).addClass("toggle-active")},function(){});jQuery(".toggle").click(function(){jQuery(this).next(".toggle-box").slideToggle()})})
