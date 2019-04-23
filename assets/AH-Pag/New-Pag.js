4045a23ece6d66a709cfe7dc9011e9baa8ddc9c5
Uelisson-BS <welisson55@hotmail.com>
2019-04-23 14:59:56 -0300

    Update New-Pag.js

--- assets/AH-Pag/New-Pag.js
+++ assets/AH-Pag/New-Pag.js
@@ -4,3 +4,8 @@ this.showPage=function(pageNumber){if(!this.inited){alert("not inited");return}
 var oldPageAnchor=document.getElementById('pg'+this.currentPage);oldPageAnchor.className='pg-normal';this.currentPage=pageNumber;var newPageAnchor=document.getElementById('pg'+this.currentPage);newPageAnchor.className='pg-selected';var from=(pageNumber-1)*itemsPerPage+1;var to=from+itemsPerPage-1;this.showRecords(from,to)}
 this.prev=function(){if(this.currentPage>1)
 this.showPage(this.currentPage-1)}
+this.next=function(){if(this.currentPage<this.pages){this.showPage(this.currentPage+1)}}
+this.init=function(){var rows=document.getElementById(tableName).rows;var records=(rows.length-1);this.pages=Math.ceil(records/itemsPerPage);this.inited=!0}
+this.showPageNav=function(pagerName,positionId){if(!this.inited){alert("not inited");return}
+var element=document.getElementById(positionId);var pagerHtml='<span onclick="'+pagerName+'.prev();" class="pg-ctrl"> &#171 </span>';for(var page=1;page<=this.pages;page++)
+pagerHtml+='<span id="pg'+page+'" class="pg-normal" onclick="'+pagerName+'.showPage('+page+');">'+page+'</span>';pagerHtml+='<span onclick="'+pagerName+'.next();" class="pg-ctrl"> &#187;</span>';element.innerHTML=pagerHtml}}
