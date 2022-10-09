2eca9803f5363c90f79181a03e79bc5dbfa7dc26
Uelisson-BS <35790317+Uelisson-BS@users.noreply.github.com>
2022-10-09 16:58:37 -0300

    Update anilist-2.js

--- assets/AH-List/anilist-2.js
+++ assets/AH-List/anilist-2.js
@@ -6,10 +6,10 @@ const data=[]
 const searchBtn=document.getElementById('submit-search')
 const searchInput=document.getElementById('search')
 const pagination=document.getElementById('pagination')
-const ITEM_PER_PAGE=12
+const ITEM_PER_PAGE=8
 const listModel=document.getElementById("btn-listModel")
 const cardModel=document.getElementById("btn-cardModel")
-let isListModel=false
+let isListModel=!1
 let page=1
 const dataPanel=document.getElementById('data-panel')
 axios.get(INDEX_URL).then((response)=>{data.push(...response.data.results)
@@ -17,8 +17,8 @@ console.log(data)
 getTotalPages(data)
 getPageData(1,data)}).catch((err)=>console.log(err))
 function displayDataList(data){let htmlContent=''
-if(isListModel===false){data.forEach(function(item,index){htmlContent+=`
-<div class="col-sm-3"><div class="card mb-2 size"><img class="card-img-top" src="${POSTER_URL}${item.imagem}" alt="Card image cap"><img class="lith" src="${POSTER_URL}${item.imagem2}"><div class="card-body movie-item-body "><h6 class="card-title">${item.titulo}</h5></div><div class="card-footer"><button class="btn btn-info btn-show-movie" data-toggle="modal" data-target="#show-movie-modal" data-id="${item.id}">Mais Informações</button></div></div></div>`})}else if(isListModel===true){data.forEach(function(item,index){htmlContent+=`<div class="container"><div class="row size"><div class="col-9"><h5>${item.titulo}</h5></div><div class="col-3 card-footer"><button class="btn btn-info btn-show-movie" data-toggle="modal" data-target="#show-movie-modal" data-id="${item.id}">Mais Informação</button></div></div></div>`})}
+if(isListModel===!1){data.forEach(function(item,index){htmlContent+=`
+<div class="col-sm-3"><div class="card mb-2 size"><img class="card-img-top" src="${POSTER_URL}${item.imagem}" alt="Card image cap"><img class="lith" src="${POSTER_URL}${item.imagem2}"><div class="card-body movie-item-body "><h6 class="card-title">${item.titulo}</h5></div><div class="card-footer"><button class="btn btn-info btn-show-movie" data-toggle="modal" data-target="#show-movie-modal" data-id="${item.id}">Mais Informações</button></div></div></div>`})}else if(isListModel===!0){data.forEach(function(item,index){htmlContent+=`<div class="container"><div class="row size"><div class="col-9"><h5>${item.titulo}</h5></div><div class="col-3 card-footer"><button class="btn btn-info btn-show-movie" data-toggle="modal" data-target="#show-movie-modal" data-id="${item.id}">Mais Informação</button></div></div></div>`})}
 dataPanel.innerHTML=htmlContent}
 function showMovie(id){const modalTitle=document.getElementById('show-movie-title')
 const modalImagem=document.getElementById('show-movie-image')
@@ -61,11 +61,11 @@ getTotalPages(results)
 getPageData(1,results)})
 cardModel.addEventListener("click",event=>{if(event.target.matches("#btn-cardModel")){listModel.classList.remove('hover-color-change')
 cardModel.classList.add('hover-color-change')
-isListModel=false
+isListModel=!1
 getPageData(page)}})
 listModel.addEventListener("click",event=>{if(event.target.matches("#btn-listModel")){cardModel.classList.remove('hover-color-change')
 listModel.classList.add('hover-color-change')
-isListModel=true
+isListModel=!0
 getPageData(page)}})
 pagination.addEventListener('click',event=>{console.log(event.target.dataset.page)
 page=event.target.dataset.page
